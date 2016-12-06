f97e478c3799f4598a06b70e03c87d1f3a138b69
Odet Alexandre <odet.alexandre.93@gmail.com>
2016-12-05 21:01:35 -0500

    Delete all protocols

---
+++ Controllers/BaseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BaseViewController.swift
+//  Nightline
+//
+//  Created by Odet Alexandre on 05/12/2016.
+//  Copyright © 2016 Odet Alexandre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+}
