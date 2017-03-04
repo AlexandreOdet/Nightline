8f6743e7c4ddde84ae14ae32c6afb9b83abfdb7f
Odet Alexandre <odet.alexandre.93@gmail.com>
2017-03-04 13:30:24 -0500

    Documentation: MapFilterManager

--- Nightline/Manager/MapFilterManager.swift
+++ Nightline/Manager/MapFilterManager.swift
@@ -8,7 +8,12 @@
 
 import Foundation
 
-class FilterManager {
+/**
+ MapFilterManager class
+ This class is here to handle how the filters will be send over the API.
+*/
+
+final class FilterManager {
   static let instance = FilterManager()
   
   var hasFilterActivated: Bool = false
@@ -17,15 +22,110 @@ class FilterManager {
   var personFilterArray = Array<PersonType>()
   var etablishmentFilterArray = Array<Etablishment>()
   
-  func add(groupeType: GroupType) {}
-  func add(eventType: EventType) {}
-  func add(personType: PersonType) {}
-  func add(etablishmentType: Etablishment) {}
+  /**
+   Method of the MapFilterManager class.
+   Add a new groupType to parameters.
+   
+   @param The GroupType you need to add.
+   
+   @return Nothing.
+   */
+  
+  func add(groupeType: GroupType) {
+    self.groupFilterArray.append(groupeType)
+  }
+
+  /**
+   Method of the MapFilterManager class.
+   Add a new EventType to parameters.
+   
+   @param The EventType you need to add.
+   
+   @return Nothing.
+   */
+
+  func add(eventType: EventType) {
+    self.eventFilterArray.append(eventType)
+  }
+  
+  /**
+   Method of the MapFilterManager class.
+   Add a new PersonType to parameters.
+   
+   @param The PersonType you need to add.
+   
+   @return Nothing.
+   */
+  
+  func add(personType: PersonType) {
+    self.personFilterArray.append(personType)
+  }
+  
+  /**
+   Method of the MapFilterManager class.
+   Add a new Etablishment to parameters.
+   
+   @param The Etablishment you need to add.
+   
+   @return Nothing.
+   */
+  
+  func add(etablishmentType: Etablishment) {
+    self.etablishmentFilterArray.append(etablishmentType)
+  }
+  
+  /**
+   Method of the MapFilterManager class.
+   Remove a groupType from parameters.
+   
+   @param The GroupType you need to remove.
+   
+   @return Nothing.
+   */
   
   func remove(groupeType: GroupType) {}
+  
+  /**
+   Method of the MapFilterManager class.
+   Remove a EventType from parameters.
+   
+   @param The EventType you need to remove.
+   
+   @return Nothing.
+   */
+  
   func remove(eventType: EventType) {}
+  
+  /**
+   Method of the MapFilterManager class.
+   Remove a PersonType from parameters.
+   
+   @param The PersonType you need to remove.
+   
+   @return Nothing.
+   */
+  
   func remove(personType: PersonType) {}
+  
+  /**
+   Method of the MapFilterManager class.
+   Remove an Etablishment from parameters.
+   
+   @param The Etablishment you need to remove.
+   
+   @return Nothing.
+   */
+  
   func remove(etablishmentType: Etablishment) {}
+  
+  /**
+   Method of the MapFilterManager class.
+   Create a Dictionary of all the filters values.
+   
+   @param None.
+   
+   @return Dictionary <String, String> of the parameters.
+   */
   
   func toParameters() -> [String:String] {
     var parameters = [String:String]()
@@ -35,6 +135,15 @@ class FilterManager {
     parameters["etabl"] = formatArrayToParametersString(array: self.etablishmentFilterArray)
     return parameters
   }
+  
+  /**
+   Method of the MapFilterManager class.
+   Format the Array<GroupType> values to specific string output.
+   
+   @param The array that needs to be formatted (Array<GroupType>).
+   
+   @return Formatted String.
+   */
   
   private func formatArrayToParametersString(array: Array<GroupType>) -> String {
     var value = ""
@@ -47,6 +156,16 @@ class FilterManager {
     return (value.isEmpty) ? "all" : value
   }
   
+  /**
+   Method of the MapFilterManager class.
+   Format the Array<EventType> values to specific string output.
+   
+   @param The array that needs to be formatted (Array<EventType>).
+   
+   @return Formatted String.
+   */
+
+  
   private func formatArrayToParametersString(array: Array<EventType>) -> String {
     var value = ""
     for item in array {
@@ -58,6 +177,16 @@ class FilterManager {
     return (value.isEmpty) ? "all" : value
   }
   
+  /**
+   Method of the MapFilterManager class.
+   Format the Array<PersonType> values to specific string output.
+   
+   @param The array that needs to be formatted (Array<PersonType>).
+   
+   @return Formatted String.
+   */
+
+  
   private func formatArrayToParametersString(array: Array<PersonType>) -> String {
     var value = ""
     for item in array {
@@ -68,6 +197,16 @@ class FilterManager {
     }
     return (value.isEmpty) ? "all" : value
   }
+  
+  /**
+   Method of the MapFilterManager class.
+   Format the Array<Etablishment> values to specific string output.
+   
+   @param The array that needs to be formatted (Array<Etablishment>).
+   
+   @return Formatted String.
+   */
+
   
   private func formatArrayToParametersString(array: Array<Etablishment>) -> String {
     var value = ""
