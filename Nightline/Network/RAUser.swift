6a1e6ccb20a76d5d1d1ef830de07424d4853b27e
xploit <xploit@hook.events>
2017-03-22 00:06:18 +0800

    Tentative de debug du login

--- Nightline/Network/RAUser.swift
+++ Nightline/Network/RAUser.swift
@@ -23,15 +23,15 @@ class RAUser: RABase {
    @param email: user's mail, password: user's password, callback: the closure called when the request succeed, callbackError: the closure called when the request fail.
    @return None
    */
-  func loginUser(email: String, password: String,
-                 callback: @escaping (User) -> (),
-                 callbackError: @escaping () -> ()) {
-    
-    let headers = ["Content-Type":"application/json"]
-    let parameters = ["email":"test@test.com", "password":"test"]
-    let url = RoutesAPI.login.url
-    print("URL = \(url)")
-    self.request = Alamofire.request(url, method: .post, parameters: parameters, headers: headers).response(completionHandler: {response in log.debug("\(response)")})
+//  func loginUser(email: String, password: String,
+//                 callback: @escaping (User) -> (),
+//                 callbackError: @escaping () -> ()) {
+//    
+//    let headers = ["Content-Type":"application/json"]
+//    let parameters = ["email":"test@test.com", "password":"test"]
+//    let url = RoutesAPI.login.url
+//    print("URL = \(url)")
+//    self.request = Alamofire.request(url, method: .post, parameters: parameters, headers: headers).response(completionHandler: {response in log.debug("\(response)")})
 //      .responseObject(completionHandler: { (response: DataResponse<User>) in
 //      switch response.result {
 //      case .success(let token):
@@ -42,6 +42,65 @@ class RAUser: RABase {
 //        log.error("Fail : \(error)")
 //      }
 //    })
+//  }
+  
+  func loginUser(email: String, password: String,
+                 callback: @escaping (User) -> (),
+                 callbackError: @escaping () -> ()) {
+    let todosEndpoint: String = "https://api.nightline.fr/login"
+    guard let todosURL = URL(string: todosEndpoint) else {
+      print("Error: cannot create URL")
+      return
+    }
+    var todosUrlRequest = URLRequest(url: todosURL)
+    todosUrlRequest.httpMethod = "POST"
+    let parameters: [String: Any] = ["email": email, "password": password]
+    let jsonTodo: Data
+    do {
+      jsonTodo = try JSONSerialization.data(withJSONObject: parameters, options: [])
+      todosUrlRequest.httpBody = jsonTodo
+    } catch {
+      print("Error: cannot create JSON from response")
+      return
+    }
+    
+    let session = URLSession.shared
+    
+    let task = session.dataTask(with: todosUrlRequest) {
+      (data, response, error) in
+      guard error == nil else {
+        print("error calling POST on /login")
+        print(error!)
+        return
+      }
+      guard let responseData = data else {
+        print("Error: did not receive data")
+        return
+      }
+      
+      print("ResponseData is = \(responseData)")
+      do {
+        print("ReceivedData: \(responseData)")
+        guard let response = try JSONSerialization.jsonObject(with: responseData,
+                                                              options: []) as? [String: Any] else {
+                                                                print("Could not get JSON from responseData as dictionary")
+                                                                return
+        }
+        print("The todo is: " + response.description)
+        print("Responses keys = \(response.keys)")
+        print("Token = \(response["token"])")
+        guard let token = response["token"] else {
+          print("Could not get token as string from JSON")
+          return
+        }
+        print("The token is: \(token)")
+        
+      } catch  {
+        print("error parsing response from POST on /todos")
+        return
+      }
+    }
+    task.resume()
   }
 
   /*
