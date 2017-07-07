f1421a7be07cd4b6ca0ad581ceeac2d59c89a7e1
Odet Alexandre <odet.alexandre.93@gmail.com>
2017-07-07 16:44:48 +0200

    Fake bar menu

---
+++ Nightline/Models/Parties/Consommable.swift
@@ -0,0 +1,14 @@
+//
+//  Consommable.swift
+//  Nightline
+//
+//  Created by Odet Alexandre on 07/07/2017.
+//  Copyright © 2017 Odet Alexandre. All rights reserved.
+//
+
+import Foundation
+
+class Consommable {
+  var name: String?
+  var price: Float?
+}
