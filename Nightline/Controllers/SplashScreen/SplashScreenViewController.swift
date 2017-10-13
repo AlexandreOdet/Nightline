08391ddfc9733abf31ab67ef8ff4dafbbb65d5fb
ODET Alexandre <madddogg39@gmail.com>
2017-10-13 18:00:30 +0200

    Create SplashScreen classes

---
+++ Nightline/Controllers/SplashScreen/SplashScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashScreenViewController.swift
+//  Nightline
+//
+//  Created by Odet Alexandre on 13/10/2017.
+//  Copyright © 2017 Odet Alexandre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class SplashScreenViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  func startLoadingSpinner() {
+    
+  }
+  
+  func stopLoadingSpinner() {
+    
+  }
+}
