84606879e115ec2f8ac2a43f6ecedd5fa0922e17
ODET Alexandre <madddogg39@gmail.com>
2017-11-13 16:29:53 +0100

    Create an Enum for WebSocket
    Will be filled when websockets will be available on the back-end

---
+++ Nightline/Enums/Routes/WebSocketAPI.swift
@@ -0,0 +1,15 @@
+//
+//  WebSocketAPI.swift
+//  Nightline
+//
+//  Created by Odet Alexandre on 13/11/2017.
+//  Copyright © 2017 Odet Alexandre. All rights reserved.
+//
+
+import Foundation
+
+enum WebSocketAPI {
+  case messagerie
+  case echo
+  case notification
+}
