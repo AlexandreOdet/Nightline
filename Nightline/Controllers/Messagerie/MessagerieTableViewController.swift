0cfbb3323bd7618eef76c291a32e228b1397c984
ODET Alexandre <madddogg39@gmail.com>
2017-11-22 10:38:09 +0100

    Create a TableViewController for messagerie (inc)

---
+++ Nightline/Controllers/Messagerie/MessagerieTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MessagerieTableViewController.swift
+//  Nightline
+//
+//  Created by Odet Alexandre on 22/11/2017.
+//  Copyright © 2017 Odet Alexandre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Starscream
+
+class MessagerieTableViewController: BaseViewController {
+  
+  var tableView: UITableView!
+  var textField = UITextField()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
