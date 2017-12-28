d41438c6c1b4d2ff7fa284df2ee8b94c56e78739
ODET Alexandre <madddogg39@gmail.com>
2017-12-28 18:18:26 +0100

    Début Websocket messagerie + RAInvitations

--- Nightline/Network/RAInvitations.swift
+++ Nightline/Network/RAInvitations.swift
@@ -10,28 +10,67 @@ import Foundation
 import Alamofire
 import AlamofireObjectMapper
 import ObjectMapper
+import PromiseKit
 
 class RAInvitations: RABase {
-    /*
-     POST /invitations/users/id/accept
-     POST /invitations/users/id/refuse
-     GET /users/userID/invitations
-     POST /invitations/groups/id/accept
-     POST /invitations/groups/id/refuse
-     GET /users/userID/invitations
-     GET /users/userID/groups/invitations
-     */
-  
-  func acceptUserInvitation(invitationID: String) {
-    
+
+  enum responseServer {
+    case success
+    case failure(Error)
   }
   
-  func declineUserInvitation(invitationID: String) {
-    
+  func inviteFriend(userId: String, friendId: String, callback: @escaping (responseServer) -> ()) {
+    let url = RoutesAPI.baseUrl + AppConstant.Network.user + "/\(userId)/friends/\(friendId)/invite"
+    Alamofire.request(url).responseJSON(completionHandler: {
+      response in
+      switch response.result {
+      case .failure(let error):
+        callback(.failure(error))
+      case .success(_):
+        callback(.success)
+      }
+    })
   }
   
-  func getUserInvitations(userID: String) {
-    
+  func acceptUserInvitation(invitationID: String, callback: @escaping (responseServer) -> ()) {
+    let url = RoutesAPI.baseUrl + "/invitations/users/\(invitationID)/accept"
+    Alamofire.request(url).responseJSON(completionHandler: {
+      response in
+      switch response.result {
+      case .failure(let error):
+        callback(.failure(error))
+      case .success(_):
+        callback(.success)
+      }
+    })
+  }
+  
+  func declineUserInvitation(invitationID: String, callback: @escaping (responseServer) -> ()) {
+    let url = RoutesAPI.baseUrl + "/invitations/users/\(invitationID)/decline"
+    Alamofire.request(url).responseJSON(completionHandler: {
+      response in
+      switch response.result {
+      case .failure(let error):
+        callback(.failure(error))
+      case .success(_):
+        callback(.success)
+      }
+    })
+  }
+  
+  func getUserInvitations(userID: String) -> Promise<InvitationsList> {
+    let url = RoutesAPI.baseUrl + AppConstant.Network.user + "/\(userID)/invitations/users"
+    return Promise { fulfill, reject in
+      self.request = Alamofire.request(url).responseObject(completionHandler: {
+        (response: DataResponse<InvitationsList>) in
+        switch response.result {
+        case .failure(let error):
+          reject(error)
+        case .success(let invitations):
+          fulfill(invitations)
+        }
+      })
+    }
   }
   
   func acceptGroupInvitation(invitationID: String) {
