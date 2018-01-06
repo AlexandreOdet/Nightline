8099407f32acb4d6678e6f1574d2c200c56833f7
xploit <cedric1.moreaux@epitech.eu>
2018-01-06 23:46:12 +0100

    EditProfile -> age become datePicket for Birthdate

--- Nightline/Controllers/Settings/EditFieldViewControllers/EditAgeViewController.swift
+++ Nightline/Controllers/Settings/EditFieldViewControllers/EditAgeViewController.swift
@@ -11,76 +11,74 @@ import UIKit
 import SnapKit
 import Rswift
 
-class EditAgeViewController: BaseViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-  
-  let contentView = UIView()
-  let editLabel = UILabel()
-  let agePicker = UIPickerView()
-  
-  func setupView() {
-    self.view.addSubview(contentView)
-    contentView.snp.makeConstraints { (make) -> Void in
-      make.top.equalTo(self.view).offset((self.navigationController?.navigationBar.frame.height)! + UIApplication.shared.statusBarFrame.height)
-      make.left.right.bottom.equalTo(self.view)
+class EditAgeViewController: BaseViewController {
+
+    let contentView = UIView()
+    let editLabel = UILabel()
+    let datePicker = UIDatePicker()
+
+    func setupView() {
+        self.view.addSubview(contentView)
+        contentView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view).offset((self.navigationController?.navigationBar.frame.height)! + UIApplication.shared.statusBarFrame.height)
+            make.left.right.bottom.equalTo(self.view)
+        }
+        self.contentView.addSubview(editLabel)
+        editLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView).offset(20)
+            make.leading.equalTo(contentView).offset(15)
+            make.trailing.equalTo(contentView).offset(-15)
+            make.height.equalTo(30)
+        }
+        self.view.backgroundColor = self.backgroundColor()
+        editLabel.text = "Date de naissance :"
+        editLabel.textColor = self.labelTextColor()
+        self.contentView.addSubview(datePicker)
+        datePicker.snp.makeConstraints { make in
+            make.top.equalTo(editLabel.snp.bottom).offset(20)
+            make.right.left.equalTo(self.contentView)
+            make.height.equalTo(self.contentView).dividedBy(2)
+        }
+        setDatePicker()
     }
-    self.contentView.addSubview(editLabel)
-    editLabel.snp.makeConstraints { make in
-      make.top.equalTo(contentView).offset(20)
-      make.leading.equalTo(contentView).offset(15)
-      make.trailing.equalTo(contentView).offset(-15)
-      make.height.equalTo(30)
+
+    func setDatePicker() {
+        datePicker.calendar = Calendar.current
+        datePicker.datePickerMode = .date
+        datePicker.maximumDate = Date()
+        if let actualUserBD = UserManager.instance.getBirthDate() {
+            datePicker.setDate(actualUserBD, animated: true)
+        } else {
+            let date = Calendar.current.date(byAdding: .year, value: -18, to: Date())
+            datePicker.setDate(date!, animated: true)
+        }
+        datePicker.backgroundColor = self.backgroundColor()
+        datePicker.tintColor = .orange
+        datePicker.setValue(UIColor.orange, forKeyPath: "textColor")
     }
-    self.view.backgroundColor = self.backgroundColor()
-    editLabel.text = "Age :"
-    editLabel.textColor = self.labelTextColor()
-    self.contentView.addSubview(agePicker)
-    agePicker.snp.makeConstraints { make in
-      make.top.equalTo(editLabel.snp.bottom).offset(20)
-      make.right.left.equalTo(self.contentView)
-      make.height.equalTo(self.contentView).dividedBy(2)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
     }
-    self.agePicker.delegate = self
-    self.agePicker.dataSource = self
-    let myRow : Int = Int(UserManager.instance.getUserAge()) ?? 0
-    self.agePicker.selectRow(myRow, inComponent: 0, animated: true)
-    self.agePicker.backgroundColor = self.backgroundColor()
-  }
-  
-  func numberOfComponents(in pickerView: UIPickerView) -> Int {
-    return 1
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    return 99
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-    let str = row.toString()
-    return NSAttributedString(string: str, attributes: [NSAttributedStringKey.foregroundColor:UIColor.init(hex: 0xFF7F40)])
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupView()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  override func viewWillDisappear(_ animated: Bool) {
-    UserManager.instance.updateUserAge(newValue: agePicker.selectedRow(inComponent: 0).toString())
-  }
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        UserManager.instance.updateUserAge(newValue: datePicker.date)
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
 }
