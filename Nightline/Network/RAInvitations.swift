d9f9c07a9570915904cd0b14f84b43769c445c39
ODET Alexandre <madddogg39@gmail.com>
2018-01-06 14:49:59 +0100

    RAInvitation: Accept/Decline group invitation

--- Nightline/Network/RAInvitations.swift
+++ Nightline/Network/RAInvitations.swift
@@ -88,15 +88,29 @@ class RAInvitations: RABase {
     }
   }
   
-  func acceptGroupInvitation(invitationID: String) {
-    
+  func acceptGroupInvitation(invitationID: String, callback: @escaping (responseServer) -> ()) {
+    let url = RoutesAPI.baseUrl + "/invitations/groups/\(invitationID)/accept"
+    Alamofire.request(url).responseJSON(completionHandler: {
+      response in
+      switch response.result {
+      case .failure(let error):
+        callback(.failure(error))
+      case .success(_):
+        callback(.success)
+      }
+    })
   }
   
-  func declineGroupInvitation(invitationID: String) {
-    
-  }
-  
-  func getUserGroupsInvitation(userID: String) {
-    
+  func declineGroupInvitation(invitationID: String, callback: @escaping (responseServer) -> ()) {
+    let url = RoutesAPI.baseUrl + "/invitations/groups/\(invitationID)/decline"
+    Alamofire.request(url).responseJSON(completionHandler: {
+      response in
+      switch response.result {
+      case .failure(let error):
+        callback(.failure(error))
+      case .success(_):
+        callback(.success)
+      }
+    })
   }
 }
