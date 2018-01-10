8d6c8a38895012f9af6a3e51e8b84f87dca30f4e
xploit <cedric1.moreaux@epitech.eu>
2018-01-10 01:29:50 +0100

    Fix notif parser

--- Nightline/Manager/NotificationManager.swift
+++ Nightline/Manager/NotificationManager.swift
@@ -18,11 +18,11 @@ class NotificationManager {
   func buildNotification(from json: [String:Any]) -> NightlineNotification {
     let notification = NightlineNotification()
     print("json = \(json)")
-    let jsn = JSON(arrayLiteral: json)
-    if let type = jsn["name"].string,
-        let body = jsn["body"].dictionary {
+    if let type = json["name"] as? String, let  body = json["body"] as? [String:Any] {
         notification.type = type
         notification.body = body
+    } else {
+        log.debug("La notif n'a pas pu etre parsé")
     }
     return notification
   }
